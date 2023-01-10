cddb9209aad132ac903edcc953c905c8c6213b22
Alex Cho <106234080+AlexC-STC-07@users.noreply.github.com>
2023-01-10 14:48:10 +0800

    Add files via upload

---
+++ views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Contact
+
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contact = Contact(name=name, email=email, message=message)
+        contact.save()
+        # perform any other actions you need here
+    return render(request, 'contact.html')
