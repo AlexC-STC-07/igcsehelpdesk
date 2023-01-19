b5e1e237d97dcaa9af83dad529840fd15b771abe
Alex Cho <106234080+AlexC-STC-07@users.noreply.github.com>
2023-01-19 22:28:50 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', index_page)
+@app.route('/')
 def index_page():
-    return "test flask shit"
+    return "This is IGCSE/MYP Help Desk!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
 
-#hi
+
+
